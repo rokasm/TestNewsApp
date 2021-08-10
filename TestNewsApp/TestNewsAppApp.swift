9502a4ae3c1ba4a3ca73d9ee4d33d81f1dbf9a98
rokas_m <rokas1899@gmail.com>
2021-08-10 11:56:48 +0300

    Initial Commit

---
+++ TestNewsApp/TestNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewsAppApp.swift
+//  TestNewsApp
+//
+//  Created by Rokas Mikelionis on 2021-08-10.
+//
+
+import SwiftUI
+
+@main
+struct TestNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
